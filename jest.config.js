849109e98ff275614a3cfa5da575abf0aa374752
ervitis <vitomarti@gmail.com>
2018-06-03 18:37:11 +0200

    add eslint and jest config

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$",
+  "modulePathIgnorePatterns": [
+    "/__mocks__/.*"
+  ],
+  "moduleFileExtensions": [
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ]
+}
