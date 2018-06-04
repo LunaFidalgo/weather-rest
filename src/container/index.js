798a51b348025011fc2d5a39d3780996305d161d
ervitis <vitomarti@gmail.com>
2018-06-04 22:16:40 +0200

    container load modules

--- src/container/index.js
+++ src/container/index.js
@@ -1,10 +1,33 @@
 const { createContainer } = require('awilix')
+const { scopePerRequest } = require('awilix-express')
+const config = require('config')
 
 const container = createContainer()
 
 container
   .loadModules([
-    ''
-  ])
+    'app/Application.js',
+
+    'infra/logging/Logger.js',
+
+    'input_interfaces/http/router.js',
+    'input_interfaces/http/Server.js',
+
+    'input_interfaces/http/middleware/ErrorHandler.js',
+    'input_interfaces/http/middleware/ErrorMiddleware.js',
+    'input_interfaces/http/middleware/logger.js'
+
+  ], {
+    resolverOptions: {
+      lifetime: Lifetime.SINGLETON
+    },
+    cwd: `${__dirname}/../`
+  })
+
+container
+  .register({
+    config: asValue(config),
+    containerMiddleware: asValue(scopePerRequest(container))
+  })
 
 module.exports = container
